34c8c1eaf7a7725e0fe73b3c9fc90d792d4a665b
David Mandelberg <david@mandelberg.org>
2009-09-18 21:58:03 +0000

    make the hazards work
    svn path=/4pong/trunk/; revision=5

--- hazard.py
+++ hazard.py
@@ -8,7 +8,8 @@ class Hazard(pygame.sprite.Sprite):
     pygame.sprite.Sprite.__init__(self)
     self.image = pygame.Surface((100,100))
     self.image.fill(config.colors['bg'])
+    self.image.set_colorkey(config.colors['bg'])
     self.rect = self.image.get_rect()
     if type == Hazard.BOTTOM_RIGHT:
       self.rect = pygame.draw.line(self.image, config.colors['fg'], (0,100), (100,0), 5)
-    self.rect.move((300,300))
+      self.rect = self.rect.move((300,300))
