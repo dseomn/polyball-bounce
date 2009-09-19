ad9288e82b56a802d10ebd3f9624760d4cedf5c4
David Mandelberg <david@mandelberg.org>
2009-09-19 04:02:41 +0000

    move normalize() into setter methods
    svn path=/4pong/trunk/; revision=22

--- velocity.py
+++ velocity.py
@@ -5,10 +5,10 @@ class Velocity:
   _speed = None
 
   def get_angle(self):
+    if self._angle > 2*math.pi or self._angle < 0:
     return self._angle
   def set_angle(self, value):
-    self._angle = value
-    self.normalize()
+    self._angle = value % (2*math.pi)
   def del_angle(self):
     del self._angle
   angle = property(get_angle, set_angle, del_angle)
@@ -16,27 +16,17 @@ class Velocity:
   def get_speed(self):
     return self._speed
   def set_speed(self, value):
-    self._speed = value
-    self.normalize()
+    self._speed = abs(value)
+    if value < 0:
+      self.angle += math.pi
   def del_speed(self):
     del self._speed
   speed = property(get_speed, set_speed, del_speed)
 
   def __init__(self, speed, angle):
     "angle = angle CCW of the x+ axis in radians, 0 < angle < 2*pi"
-    self._speed = speed
-    self._angle = angle
-    self.normalize()
-
-  def normalize(self):
-    "make sure speed and angle are within parameters"
-    while self._angle < 0:
-      self._angle += 2 * math.pi
-    self._angle %= 2 * math.pi
-    if self._speed < 0:
-      self._speed = abs(self._speed)
-      self._angle += self.math.pi
-      self.normalize()
+    self.speed = speed
+    self.angle = angle
 
   def delta(self, time):
     "return (change from top, change from left) for given time"
