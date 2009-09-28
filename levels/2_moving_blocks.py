d16a9e139cea900099dd3b8aa3bafbc8b37bcfc6
David Mandelberg <david@mandelberg.org>
2009-09-28 23:19:39 +0000

    new level
    svn path=/polyball-bounce/trunk/; revision=101

---
+++ levels/2_moving_blocks.py
@@ -0,0 +1,32 @@
+import pygame
+
+def init(config):
+  square = pygame.Surface((30,30))
+  square.fill(config.colors['fg'])
+  square.set_colorkey(config.colors['bg'])
+  def update(self):
+    if not hasattr(self, 'dir'):
+      self.dir = (2,0)
+    if self.rect.centery < 120:
+      self.rect.centery = 120
+      self.dir = (2,0)
+    elif self.rect.centerx > 280:
+      self.rect.centerx = 280
+      self.dir = (0,2)
+    elif self.rect.centery > 280:
+      self.rect.centery = 280
+      self.dir = (-2,0)
+    elif self.rect.centerx < 120:
+      self.rect.centerx = 120
+      self.dir = (0,-2)
+    self.rect.centerx += self.dir[0]
+    self.rect.centery += self.dir[1]
+  
+
+  config.ball['num'] = 4
+  config.ball['speed'] = 2
+  config.hazard['custom'] = [
+    {'rect': (110, 110, 30, 30), 'image': square, 'update': update},
+    {'rect': (390, 390, 30, 30), 'image': square, 'update': update},
+  ]
+  config.paddle['speed'] = 1.25
