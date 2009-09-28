787994f5247784936a52f3a154a459594c67ea5d
David Mandelberg <david@mandelberg.org>
2009-09-28 04:33:52 +0000

    if the object has no attribute normal, compute one based on how the objects collided
    svn path=/polyball-bounce/trunk/; revision=80

--- util.py
+++ util.py
@@ -1,5 +1,29 @@
 import math
+import pygame
 
 def bounce_angle(self, other):
   "compute the angle other should bounce off self"
-  return 2*self.normal - other.vel.angle + math.pi
+
+  if hasattr(self, 'normal'):
+    normal = self.normal
+  else:
+    for o in (self, other):
+      if not hasattr(o, 'mask'):
+        o.mask = pygame.mask.from_surface(o.image)
+    x = other.rect.left - self.rect.left
+    y = other.rect.top - self.rect.top
+    dx = self.mask.overlap_area(other.mask,(x-1,y)) - self.mask.overlap_area(other.mask,(x+1,y))
+    dy = self.mask.overlap_area(other.mask,(x,y+1)) - self.mask.overlap_area(other.mask,(x,y-1))
+    if dx == 0:
+      if dy > 0:
+        normal = math.pi/2
+      else:
+        normal = 3*math.pi/2
+    else:
+      normal = math.atan(float(dy)/dx)
+      if dx < 0:
+        normal += math.pi
+      if normal < 0:
+        normal += 2*math.pi
+
+  return 2*normal - other.vel.angle + math.pi
